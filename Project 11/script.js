ec04a5c95643eb1df0e1d3d96345656534f0a044
Uditya Pal <149376004+Aman-Pal-1@users.noreply.github.com>
2024-09-13 23:37:33 +0530

    impo change

---
+++ Project 11/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const shopNowBtn = document.querySelector('.shop-now-btn');
+    shopNowBtn.addEventListener('click', () => {
+        alert('Shop Now button clicked!');
+    });
+});
